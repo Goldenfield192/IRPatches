80ce840cc301497df96000027b6e850856e1b2a2
Goldenfield192 <1437356849@qq.com>
2025-02-09 15:54:41 +0800

    Feat: Switch LICENSE to LGPL-2.1

--- src/main/java/com/goldenfield192/irpatches/mixins/MixinEntityRollingStockDefinition.java
+++ src/main/java/com/goldenfield192/irpatches/mixins/MixinEntityRollingStockDefinition.java
@@ -1,7 +1,5 @@
 package com.goldenfield192.irpatches.mixins;
 
-import cam72cam.immersiverailroading.library.ModelComponentType;
-import cam72cam.immersiverailroading.model.StockModel;
 import cam72cam.immersiverailroading.registry.EntityRollingStockDefinition;
 import cam72cam.immersiverailroading.util.DataBlock;
 import org.spongepowered.asm.mixin.Final;
@@ -9,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -23,7 +20,7 @@ public class MixinEntityRollingStockDefinition {
     public List<String> itemGroups;
 
     @Inject(method = "<init>", at = @At("TAIL"), remap = false)
-    public void mixin(Class type, String defID, DataBlock data, CallbackInfo ci){
+    public <T> void mixin(Class<T> type, String defID, DataBlock data, CallbackInfo ci){
         List<String> list = itemGroups.stream().filter(x -> !x.contains("TV_default") && x.contains("_TV_")).collect(Collectors.toList());
         this.itemGroups.removeAll(list);
     }
