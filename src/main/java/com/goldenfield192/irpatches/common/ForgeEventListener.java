0b27a14f7c137eb0b7934557cdde4ac1becf6fa2
Goldenfield192 <1437356849@qq.com>
2025-03-18 06:45:30 +0800

    fix: unexpected event call on server side

--- src/main/java/com/goldenfield192/irpatches/common/ForgeEventListener.java
+++ src/main/java/com/goldenfield192/irpatches/common/ForgeEventListener.java
@@ -5,9 +5,12 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber(modid = IRPatches.MODID)
 public class ForgeEventListener {
+    @SideOnly(Side.CLIENT)
     @SubscribeEvent
     public static void setOnboardFOV(EntityViewRenderEvent.FOVModifier event) {
          if(OnboardCamera.enabled && Minecraft.getMinecraft().gameSettings.thirdPersonView != 0){
