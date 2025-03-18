e93a1f44c708184482ac46fa6dc4361c8e820835
Goldenfield192 <1437356849@qq.com>
2025-03-19 06:19:02 +0800

    fix: remake inject of MixinRailSettingMapper.java

--- src/main/java/com/goldenfield192/irpatches/mixins/immersiverailroading/items/nbt/MixinRailSettingMapper.java
+++ src/main/java/com/goldenfield192/irpatches/mixins/immersiverailroading/items/nbt/MixinRailSettingMapper.java
@@ -6,49 +6,38 @@ import com.goldenfield192.irpatches.accessor.IRailSettingsAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 
 @Mixin(RailSettings.Mapper.class)
 public class MixinRailSettingMapper {
-    @Inject(method = "apply",
-            at = @At(value = "HEAD"),
-            remap = false, cancellable = true)
-    //We want to save some extra data......However I found it errors when injecting into lambda
-    public void overwriteSave(Class<RailSettings> type, String fieldName, TagField tag, CallbackInfoReturnable<TagMapper.TagAccessor<RailSettings>> cir){
-        cir.setReturnValue(new TagMapper.TagAccessor<>(
-                (d, o) -> {
-                    TagCompound target = new TagCompound();
-                    try {
-                        TagSerializer.serialize(target, o.mutable());
-                    } catch(SerializationException e) {
-                        // This is messy
-                        throw new RuntimeException(e);
-                    }
-                    d.set(fieldName, target);
-                },
-                d -> {
-                    try {
-                        Constructor<RailSettings.Mutable> constructor = RailSettings.Mutable.class.getDeclaredConstructor(TagCompound.class);
-                        constructor.setAccessible(true);
-                        RailSettings m = constructor.newInstance(d.get(fieldName)).immutable();
-                        if(d.get(fieldName).getFloat("ctrl1") != null){
-                            ((IRailSettingsAccessor) m).IRPatch$setFarEnd(d.get(fieldName).getFloat("ctrl1"));
-                        } else {
-                            ((IRailSettingsAccessor) m).IRPatch$setFarEnd(0);
-                        }
-                        if(d.get(fieldName).getFloat("ctrl2") != null){
-                            ((IRailSettingsAccessor) m).IRPatch$setNearEnd(d.get(fieldName).getFloat("ctrl2"));
-                        } else {
-                            ((IRailSettingsAccessor) m).IRPatch$setNearEnd(0);
-                        }
-                        return m;
-                    } catch(NoSuchMethodException | InvocationTargetException |
-                            InstantiationException | IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-                }));
+    @Inject(method = "lambda$apply$0", at = @At("RETURN"), remap = false)
+    private static void lambda0(String fieldName, TagCompound d, RailSettings o, CallbackInfo ci){
+        TagCompound tag = new TagCompound();
+        tag.setFloat("ctrl1", ((IRailSettingsAccessor)o).IRPatch$getFarEndTilt());
+        tag.setFloat("ctrl2", ((IRailSettingsAccessor)o).IRPatch$getNearEndTilt());
+        d.set("irp", tag);
+    }
+
+    @Inject(method = "lambda$apply$1", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void lambda1(String fieldName, TagCompound d, CallbackInfoReturnable<RailSettings> cir){
+        Constructor<RailSettings.Mutable> constructor;
+        RailSettings m;
+        try {
+            constructor = RailSettings.Mutable.class.getDeclaredConstructor(TagCompound.class);
+            constructor.setAccessible(true);
+            m = constructor.newInstance(d.get(fieldName)).immutable();
+        } catch(NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException  e) {
+            throw new RuntimeException(e);
+        }
+        Optional<Float> ctrl1 = Optional.ofNullable(d.get(fieldName).getFloat("ctrl1"));
+        ((IRailSettingsAccessor) m).IRPatch$setFarEnd(ctrl1.orElse(0.0f));
+        Optional<Float> ctrl2 = Optional.ofNullable(d.get(fieldName).getFloat("ctrl2"));
+        ((IRailSettingsAccessor) m).IRPatch$setNearEnd(ctrl2.orElse(0.0f));
+        cir.setReturnValue(m);
     }
 }
