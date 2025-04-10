97af4edf37e55fddfbf43c09e5ae6943395e3dc0
Goldenfield192 <1437356849@qq.com>
2025-04-11 06:25:31 +0800

    Fix: throttle jumps between notches when using augment(issue#1526 in IR)

--- src/main/java/com/goldenfield192/irpatches/mixins/immersiverailroading/entity/MixinLocomotiveDiesel.java
+++ src/main/java/com/goldenfield192/irpatches/mixins/immersiverailroading/entity/MixinLocomotiveDiesel.java
@@ -1,16 +1,20 @@
 package com.goldenfield192.irpatches.mixins.immersiverailroading.entity;
 
+import cam72cam.immersiverailroading.entity.Locomotive;
 import cam72cam.immersiverailroading.entity.LocomotiveDiesel;
 import cam72cam.immersiverailroading.registry.LocomotiveDieselDefinition;
 import cam72cam.immersiverailroading.util.BurnUtil;
 import cam72cam.mod.fluid.Fluid;
 import com.goldenfield192.irpatches.util.ExtraDefinition;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -18,7 +22,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Mixin(LocomotiveDiesel.class)
-public abstract class MixinLocomotiveDiesel {
+public abstract class MixinLocomotiveDiesel extends Locomotive {
     @Shadow(remap = false)
     public abstract LocomotiveDieselDefinition getDefinition();
 
@@ -42,5 +46,12 @@ public abstract class MixinLocomotiveDiesel {
             return overrides.stream().filter(pair -> pair.getLeft().equals(fluid.ident)).findFirst().get().getRight();
         }
         return 0;
+    }
+
+    //TODO Need research
+    @Inject(method = "setThrottle", at = @At(value = "INVOKE_ASSIGN", target = "Lcam72cam/immersiverailroading/registry/LocomotiveDieselDefinition;getThrottleNotches()I"), remap = false, cancellable = true)
+    public void inject(float newThrottle, CallbackInfo ci, @Local LocalIntRef notches){
+        super.setThrottle((float)(Math.round((double)(newThrottle * (float)notches.get())) / (double)notches.get()));
+        ci.cancel();
     }
 }
