04398da0fd29d61d2f23c60227ca2ee3779d3310
Goldenfield192 <1437356849@qq.com>
2025-04-13 15:14:05 +0800

    refactor: change branch name

--- src/main/java/com/goldenfield192/irpatches/mixins/immersiverailroading/render/item/MixinTrackBluePrintItemModel.java
+++ src/main/java/com/goldenfield192/irpatches/mixins/immersiverailroading/render/item/MixinTrackBluePrintItemModel.java
@@ -2,12 +2,21 @@ package com.goldenfield192.irpatches.mixins.immersiverailroading.render.item;
 
 import cam72cam.immersiverailroading.render.item.TrackBlueprintItemModel;
 import cam72cam.immersiverailroading.util.BlockUtil;
+import cam72cam.mod.entity.Player;
+import cam72cam.mod.item.ItemStack;
+import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.math.Vec3i;
+import cam72cam.mod.render.GlobalRender;
+import cam72cam.mod.render.opengl.RenderState;
 import cam72cam.mod.world.World;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.block.BlockLeaves;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 /**
  * Prevent leaves being replaced by track blueprint.
@@ -22,5 +31,19 @@ public class MixinTrackBluePrintItemModel {
     private static boolean mixinBlockCheck(World world, Vec3i pos, boolean flag) {
         return BlockUtil.canBeReplaced(world, pos, flag) &&
                 !(world.internal.getBlockState(pos.internal()).getBlock() instanceof BlockLeaves);
+    }
+
+    @ModifyArg(method = "renderMouseover", at = @At(value = "INVOKE", target = "Lcam72cam/immersiverailroading/render/rail/RailRender;render(Lcam72cam/immersiverailroading/util/RailInfo;Lcam72cam/mod/world/World;Lcam72cam/mod/math/Vec3i;ZLcam72cam/mod/render/opengl/RenderState;)V"), index = 4, remap = false)
+    private static RenderState modArgs(RenderState state, @Share("state")LocalRef<RenderState> stateLocalRef){
+        stateLocalRef.set(state);
+        return state;
+    }
+
+    //TODO
+    @Inject(method = "renderMouseover", at = @At("TAIL"), remap = false)
+    private static void inject(Player player, ItemStack stack, Vec3i pos, Vec3d vec, RenderState state,
+                               float partialTicks, CallbackInfo ci, @Local(ordinal = 2) Vec3d offset, @Share("state") LocalRef<RenderState> stateLocalRef){
+        if(stateLocalRef.get() == null) return;
+//        GlobalRender.drawText("Test", stateLocalRef.get(), new Vec3d(0,0,0), 0.2f, 0);
     }
 }
