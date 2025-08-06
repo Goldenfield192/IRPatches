2e0a5bc2340a651fb468a961c78623184b2e2c1f
Goldenfield192 <1437356849@qq.com>
2025-08-06 09:53:05 +0800

    fix: change transfer table rail length

--- src/main/java/com/goldenfield192/irpatches/util/BuilderTransferTable.java
+++ src/main/java/com/goldenfield192/irpatches/util/BuilderTransferTable.java
@@ -74,7 +74,7 @@ public class BuilderTransferTable extends BuilderBase {
         }
 
         Vec3d vec = new Vec3d(-info.tablePos, 1, info.settings.length / 2d - 0.5).rotateYaw(-info.placementInfo.facing().getAngle() + 180);
-        list.add(new VecYawPitch(vec.x, vec.y, vec.z, info.placementInfo.facing().getAngle(), 0, info.settings.length - 1, "RAIL_RIGHT", "RAIL_LEFT"));
+        list.add(new VecYawPitch(vec.x, vec.y, vec.z, info.placementInfo.facing().getAngle(), 0, info.settings.length, "RAIL_RIGHT", "RAIL_LEFT"));
         return list;
     }
 
